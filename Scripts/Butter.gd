532d813b854d2fcf0e76777d7b3f181a9a7a76fe
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-18 21:24:34 +0000

    Applied the mouse hovering functionality across the different models

--- Scripts/Butter.gd
+++ Scripts/Butter.gd
@@ -1,21 +1,24 @@
 extends Node3D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			print("You clicked flour")
-			
+
+func _on_area_3d_mouse_entered():
+	print("You are hovering over Butter")
 	pass # Replace with function body.
 
 
-func _on_area_3d_mouse_entered():
-	print("You are hovering over Flour")
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("You clicked butter")
+			
 	pass # Replace with function body.
--- Scripts/MixingBowl.gd
+++ Scripts/MixingBowl.gd
@@ -15,4 +15,5 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("You added item to the bowl")
+			
 	pass # Replace with function body.
