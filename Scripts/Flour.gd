c800d9235ed0ab81dd71f8e9c396c3451c5e0b4e
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-18 21:18:55 +0000

    Updated models and added mouse hovering for testing

--- Scripts/Flour.gd
+++ Scripts/Flour.gd
@@ -1,8 +1,16 @@
 extends Node3D
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			print("Flour was clicked")
+			print("You clicked flour")
 			
 	pass # Replace with function body.
--- Scripts/MixingBowl.gd
+++ Scripts/MixingBowl.gd
@@ -9,17 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-	
+
+
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			print("You clicked eggs")
-			
-	pass # Replace with function body.
-	
-
-
-func _on_area_3d_mouse_entered():
-	print("You are hovering over Eggs")
+			print("You added item to the bowl")
 	pass # Replace with function body.
