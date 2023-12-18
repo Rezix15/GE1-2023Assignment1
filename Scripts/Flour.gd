3917251d5774065caf0442ee6754829bf5b0974b
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-18 15:43:02 +0000

    Created a testing script that detects mouse input on given area

--- Scripts/Flour.gd
+++ Scripts/Flour.gd
@@ -1,11 +1,8 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("Flour was clicked")
+			
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
