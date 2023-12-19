913cfaaac620bc2288d2d4452d1e1d7759e45864
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-19 18:45:41 +0000

    Added the features to the eggs that were missing

--- Scripts/Egg.gd
+++ Scripts/Egg.gd
@@ -1,13 +1,18 @@
 extends Node3D
+var itemCount:int = 1
 
+signal eggSelected(isSelected)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	itemCount = 1
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if itemCount <= 0:
+		visible = false
 	pass
 	
 	
@@ -15,7 +20,10 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("You clicked eggs")
-			visible = false
+			
+			if itemCount > 0:
+				itemCount = itemCount - 1
+				emit_signal("eggSelected", true)
 			
 	pass # Replace with function body.
 	
