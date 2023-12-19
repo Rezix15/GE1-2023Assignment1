fd652ef3e0dbc66efe14ab8e103da932c3f6af60
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-19 18:17:45 +0000

    Added the features to the eggs that were missing

--- Scripts/Egg.gd
+++ Scripts/Egg.gd
@@ -15,6 +15,7 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("You clicked eggs")
+			visible = false
 			
 	pass # Replace with function body.
 	
