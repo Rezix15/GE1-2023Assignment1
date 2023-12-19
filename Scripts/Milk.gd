0f4a93aeb184d4c84203ace84fb9977636e1529d
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-19 16:13:47 +0000

    Added the relevant nodes to the milk bottle and the relevant script

--- Scripts/Milk.gd
+++ Scripts/Milk.gd
@@ -1,5 +1,5 @@
 extends Node3D
-
+var itemCount:int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +9,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("You clicked the milk bottle")
+					
+			if itemCount > 0:	
+				itemCount = itemCount - 1
+			else:
+				print("Milk bottle is empty")
+	pass # Replace with function body.
+
+
+func _on_area_3d_mouse_entered():
+	print("You are hovering over the milk bottle")
+	pass # Replace with function body.
