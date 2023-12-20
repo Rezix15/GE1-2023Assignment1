b5929ebcb5469587cad042b1c49f22cdff4e7a25
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-20 04:16:53 +0000

    Added in a switch to control the stove

--- Scripts/CameraControl3D.gd
+++ Scripts/CameraControl3D.gd
@@ -9,14 +9,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("TurnRight"):
-		print("Right pressed")
-		global_position = Vector3(20, 12.873, 20)
+		global_position = Vector3(20, 15.873, 20)
 		rotate_y(deg_to_rad(-90))
 		# global_rotate(basis.y, -90)
 		pass
 	
 	if Input.is_action_just_pressed("TurnLeft"):
-		print("Left pressed")
 		global_position = Vector3(0, 9.873, 11)
 		rotate_y(deg_to_rad(90))
 		pass
---
+++ Scripts/Switch.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+var cycleRot:float = 0
+var status:bool
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	status = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			cycleRot += deg_to_rad(180)
+			rotate_z(cycleRot)
+			print(rad_to_deg(cycleRot))
+			status = !status
+			print(status)
+			
+	pass # Replace with function body.
+
+
+func _on_area_3d_mouse_entered():
+	print("Stove Switch")
+	pass # Replace with function body.
