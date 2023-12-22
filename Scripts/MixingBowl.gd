984d2a73470efaccc2d8f66ac73a04d49555775d
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2023-12-22 15:13:10 +0000

    Added the score system and objective list to the pancake section

--- Scripts/MixingBowl.gd
+++ Scripts/MixingBowl.gd
@@ -3,51 +3,60 @@ extends Node3D
 var itemBowl = []
 signal mixItems()
 signal readyToMix()
+var hasMixed:bool
 var mixed:bool
+var score:int = 0
+var scoreText
+var objectiveText
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mixed = false
-	pass # Replace with function body.
-
+	scoreText = get_node("../Control/ScorePanel/ScoreText")
+	objectiveText = get_node("../Control/ObjectivePanel/CurrentObjectiveText")
+	
+	objectiveText.text = "1.  Add Ingredients To The Mixing Bowl"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if itemBowl.size() >= 4 and mixed == false:
+	scoreText.text = str(score)
+	if itemBowl.size() >= 4 and mixed == false and hasMixed == false:
+		objectiveText.text = "2. Stir the batter with the whisk"
 		emit_signal("readyToMix")
-	pass
 
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true and itemBowl.size() >= 4:
 			pass
-			
-	pass # Replace with function body.
 
 
 func _on_egg_egg_selected(isSelected):
 	var egg1 = "Egg1"
-	itemBowl.append(egg1) 
-	pass # Replace with function body.
+	itemBowl.append(egg1)
+	score += 10 
+	
 
 
 func _on_egg_2_egg_selected(isSelected):
 	var egg2 = "Egg2"
 	itemBowl.append(egg2)
-	pass # Replace with function body.
+	score += 10
+	
 
 
 func _on_flour_flour_clicked(isSelected):
 	var flour = "Flour"
 	itemBowl.append(flour)
-	pass # Replace with function body.
+	score += 10
+	
 
 
 func _on_milk_milk_clicked(isSelected):
 	var milk = "Milk"
 	itemBowl.append(milk)
-	pass # Replace with function body.
+	score += 10
+
 
 
 func _on_area_3d_mouse_entered():
@@ -55,11 +64,13 @@ func _on_area_3d_mouse_entered():
 	
 	if itemBowl.size() >= 4 and mixed == false:
 		print("Click to mix")
-	pass # Replace with function body.
 
 
 func _on_whisk_mix_items():
 	print("Mixing process")
 	mixed = true
 	emit_signal("mixItems")
-	pass # Replace with function body.
+	if hasMixed == false:
+		score += 100
+		hasMixed = true
+		objectiveText.text = "3. Move to the right and fry your pancakes on the pan"
